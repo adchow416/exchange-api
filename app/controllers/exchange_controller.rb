1911975c1ad8e14ffc703fda4ff27200eb71c5b0
adchow416 <36019769+adchow416@users.noreply.github.com>
2021-11-21 16:05:53 -0500

    Setup backend
    Setup backend

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -1,4 +1,17 @@
 class ExchangeController < ApplicationController
-    def test
+    #before_action :validate_params, only: [:convert_currency]
+    def currency_options
+        render json: ["US", "CAN"]
     end
+
+    def convert_currency
+        puts params
+        render json: 1
+    end
+
+    private
+
+    # def validate_params
+    #     params.require(:post).permit(:title, :text, :slug)
+    # end
 end
